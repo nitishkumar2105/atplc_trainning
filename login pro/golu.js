0a1bcbbbb649bece5a4eacd2df7d1bee3b3e47da
nitishkumar2105 <nitishkr21052003@gmail.com>
2023-04-20 16:41:04 +0530

    assignment

---
+++ login pro/golu.js
@@ -0,0 +1,22 @@
+function login(){
+
+    // let username=document.getElementById("username");= sirf ye likhne se function keval run hota hai 
+    // let username=document.getElementById("username").value; = .value lagane pe value milta hai 
+    let username=document.getElementById("username").value;
+    let password=document.getElementById("password").value;
+
+
+    if (username=='MCEIAN'&& password=='MCE01') {
+        console.log('login succesfully');
+        location.assign('https://www.mcemotihari.ac.in/'); // ye kisi dusre page/path me lekar jata hai
+    }
+    else(
+        window.alert('wrong username and password!') // ye ek pup up show karta hai ki aapka password wrong h ai
+    )
+
+
+
+    console.log(username);
+    console.log(password);
+}
+login()
